1d7f0250936f533031bc556b7d9021cf2eb485d1
Emeka Moda <146528163+Moda-Emeka-BSCS32E1@users.noreply.github.com>
2024-05-28 11:00:33 +0800

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_ALBINO_WARREN_BSCS_32E.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Pokemon.cs
@@ -0,0 +1,9 @@
+﻿namespace PREFINAL_ASSIGNMENT_TWO_POKEMON_ALBINO_WARREN_BSCS_32E.Models
+{
+    public class Pokemon
+    {
+        public string Name { get; set; }
+        public List<string> Moves { get; set; }
+        public List<string> Abilities { get; set; }
+    }
+}
